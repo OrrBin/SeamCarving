08162cbcc1bbbf3e2ea74da1a5008026d880f1ca
Orr Benyamini <orr.bob@hotmail.com>
2019-04-03 16:01:21 +0300

    asdasd

--- SeamCarving/src/seamcarving/Main.java
+++ SeamCarving/src/seamcarving/Main.java
@@ -2,6 +2,7 @@ package seamcarving;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -10,22 +11,15 @@ import javax.imageio.ImageIO;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String path = "C:\\Users\\yahav\\Desktop\\tiger.jpg";
+		if(args.length == 0 || args.length < 5) {
+			throw new IllegalArgumentException("Wrong number of arguments");
+		}
 		
-		BufferedImage img = ImageIO.read(new File(path));
-		File imgdst = new File("C:\\Users\\yahav\\Desktop\\tiger2.jpg");
-		
-		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-		
-		for(int i = 0; i < img.getWidth()/2;i++)
-	        for(int j = 0; j < img.getHeight()/2; j ++)
-	        {
-	            image.setRGB(i,j , 20);
-	        }
-		
-		img.s
-		ImageIO.write(img,"jpg", imgdst);
-
+		String inputFileName = args[0];
+		int numOfCol = Integer.valueOf(args[1]);
+		int numOfRow = Integer.valueOf(args[2]);
+		int energyType = Integer.valueOf(args[3]);
+		String outputFileName = args[4];
 	}
 
 }
